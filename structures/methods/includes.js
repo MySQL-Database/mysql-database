e1d94529653b3eccb72e8286894f2e39e54debaf
tariqkazem <tariqkazem@gmail.com>
2021-07-04 06:49:28 +0300

    Updated Includes Method

--- structures/methods/includes.js
+++ structures/methods/includes.js
@@ -5,30 +5,15 @@ const EventEmitter = require('events');
 const errors = require('../errors/strings.js');
 
 module.exports = async function(table, key, value){
-	if(!table){
-		throw new ReferenceError(errors.table.replace("{received}", table));
-	}
-	if(!key){
-		throw new ReferenceError(errors.key.replace("{received}", key));
-	}
-	if(!value){
-		throw new ReferenceError(errors.value.replace("{received}", value));
-	}
-	let data;
-	let res;
-	let tables = await this.tables();
-	if(!tables.includes(table)){
-		res = false;
-	}else{
-		data = await this.get(table, key) || [];
-		if(!Array.isArray(data)){
-			throw new ReferenceError(errors.array.replace("{key}", key));
-		}
-		if(data.includes(value)){
-			res = true;
-		}else{
-			res = false;
-		}
+	if(!table) throw new TypeError(errors.table.replace("{received}", table));
+	if(!key) throw new TypeError(errors.key.replace("{received}", key));
+	if(value == null) throw new TypeError(errors.value.replace("{received}", value));
+	
+	let res = false;
+	let data = await this.get(table, key) || [];
+	if(!Array.isArray(data)) throw new TypeError(errors.array.replace("{key}", key));
+	if(data.includes(value)){
+		res = true;
 	}
 	return res;
 }

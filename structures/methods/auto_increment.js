e60f8eb992e5e89cc1579ac897f46308dd684da1
tariqkazem <tariqkazem@gmail.com>
2021-07-04 06:50:13 +0300

    Updated Auto_Increment Method

--- structures/methods/auto_increment.js
+++ structures/methods/auto_increment.js
@@ -5,17 +5,11 @@ const EventEmitter = require('events');
 const errors = require('../errors/strings.js');
 
 module.exports = async function(table, number){
-	if(!table){
-		throw new ReferenceError(errors.table.replace("{received}", table));
-	}
-	if(!number){
-		throw new ReferenceError(errors.number.replace("{received}", number));
-	}
-	if(isNaN(number) || number < 1){
-		throw new ReferenceError(errors.numberType.replace("{received}", number));
-	}
-	let db = this.db;
-	await db.query(`ALTER TABLE ${table} AUTO_INCREMENT = ${number};`);
-	let res = await db.query(`SHOW TABLE STATUS FROM ${db.pool.config.connectionConfig.database} WHERE name LIKE '${table}';`);
-	return res[0].Auto_increment;
+	if(!table) throw new TypeError(errors.table.replace("{received}", table));
+	if(number == null) throw new TypeError(errors.number.replace("{received}", number));
+	if(isNaN(number) || number < 1) throw new TypeError(errors.numberType.replace("{received}", number));
+	
+	await this.query(`ALTER TABLE ${table} AUTO_INCREMENT = ${number};`);
+	let res = await this.stats(table);
+	return res.Auto_increment;
 }

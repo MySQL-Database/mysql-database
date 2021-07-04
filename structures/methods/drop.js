506669aaff576e3ab1013cdab27e77ce15daf1f2
tariqkazem <tariqkazem@gmail.com>
2021-07-04 06:49:41 +0300

    Updated Drop Method

--- structures/methods/drop.js
+++ structures/methods/drop.js
@@ -5,10 +5,8 @@ const EventEmitter = require('events');
 const errors = require('../errors/strings.js');
 
 module.exports = async function(table){
-	if(!table){
-		throw new ReferenceError(errors.table.replace("{received}", table));
-	}
-	let db = this.db;
-	await db.query("DROP TABLE " + table);
+	if(!table) throw new TypeError(errors.table.replace("{received}", table));
+	
+	this.db.query("DROP TABLE " + table)
 	return true;
 }

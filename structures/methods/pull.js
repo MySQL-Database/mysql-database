6fd6c4e8be312a0f6183a0dca285efb7f8893f40
tariqkazem <tariqkazem@gmail.com>
2021-07-04 06:47:53 +0300

    Updated Pull Method

--- structures/methods/pull.js
+++ structures/methods/pull.js
@@ -5,27 +5,17 @@ const EventEmitter = require('events');
 const errors = require('../errors/strings.js');
 
 module.exports = async function(table, key, value, option){
-	if(!table){
-		throw new ReferenceError(errors.table.replace("{received}", table));
-	}
-	if(!key){
-		throw new ReferenceError(errors.key.replace("{received}", key));
-	}
-	if(!value){
-		throw new ReferenceError(errors.value.replace("{received}", value));
-	}
+	if(!table) throw new TypeError(errors.table.replace("{received}", table));
+	if(!key) throw new TypeError(errors.key.replace("{received}", key));
+	if(value == null) throw new TypeError(errors.value.replace("{received}", value));
+	
 	let data = await this.get(table, key);
-	if(!data){
-		throw new ReferenceError(errors.dataNotFound.replace("{key}", key));
-	}
-	if(!Array.isArray(data)){
-		throw new ReferenceError(errors.array.replace("{key}", key));
-	}
+	if(!data) throw new TypeError(errors.dataNotFound.replace("{key}", key));
+	if(!Array.isArray(data)) throw new TypeError(errors.array.replace("{key}", key));
 	if(option && option.toLowerCase() === "all"){
 		data = data.filter(obj => obj !== value);
 	}else{
 		data.splice(data.indexOf(value), 1);
 	}
-	let res = await this.set(table, key, data);
-	return res;
+	return this.set(table, key, data);
 }

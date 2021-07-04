5ec8dd9a5aeff0610e41d99e05e14317fc241e1b
tariqkazem <tariqkazem@gmail.com>
2021-07-04 06:50:27 +0300

    Updated Add Method

--- structures/methods/add.js
+++ structures/methods/add.js
@@ -5,28 +5,19 @@ const EventEmitter = require('events');
 const errors = require('../errors/strings.js');
 
 module.exports = async function(table, key, value){
-	if(!table){
-		throw new ReferenceError(errors.table.replace("{received}", table));
-	}
-	if(!key){
-		throw new ReferenceError(errors.key.replace("{received}", key));
-	}
-	if(!value){
-		throw new ReferenceError(errors.value.replace("{received}", value));
-	}
-	if(isNaN(value) || value < 1){
-		throw new ReferenceError(errors.numberType.replace("{received}", value));
-	}
+	if(!table) throw new TypeError(errors.table.replace("{received}", table));
+	if(!key) throw new TypeError(errors.key.replace("{received}", key));
+	if(value == null) throw new TypeError(errors.value.replace("{received}", value));
+	if(isNaN(value) || value < 1) throw new TypeError(errors.numberType.replace("{received}", value));
+	
 	let res;
 	let tables = await this.tables();
 	if(!tables.includes(table)){
 		res = await this.set(table, key, value);
 	}else{
 		let data = await this.get(table, key) || 0;
-		if(isNaN(data)){
-			throw new ReferenceError(errors.notNumber.replace("{key}", key));
-		}
-		res = await this.set(table, key, data + Number(value));
+		if(isNaN(data)) throw new TypeError(errors.notNumber.replace("{key}", key));
+		res = await this.set(table, key, Number(data) + Number(value));
 	}
 	return res;
 }

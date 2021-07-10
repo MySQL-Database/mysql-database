b33a9664232f826e97fdbb936002db8e8e13904f
tariqkazem <tariqkazem@gmail.com>
2021-07-10 08:55:15 +0300

    Updated Create Method

--- structures/methods/create.js
+++ structures/methods/create.js
@@ -7,7 +7,6 @@ const errors = require('../errors/strings.js');
 module.exports = async function(table){
 	if(!table) throw new TypeError(errors.table.replace("{received}", table));
 	
-	await this.set(table, "initialized_at", new Date().toLocaleString());
-	await this.delete(table, "initialized_at");
+	await this.query('CREATE TABLE IF NOT EXISTS `' + table + '` (`id` int(11) NOT NULL auto_increment, `key_name` LONGTEXT NULL, `value` LONGTEXT NULL, `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`))');
 	return true;
 }

cd5c961a774aeeebb31288687fc7a3cd980f0aca
abdooo9 <76847516+abdooo9@users.noreply.github.com>
2021-08-13 22:58:17 +0300

    Update Get Method

--- structures/methods/get.js
+++ structures/methods/get.js
@@ -3,35 +3,72 @@
 const get = require('lodash/get');
 const errors = require('../errors/strings.js');
 
-module.exports = async function(table, key){
-	if(!table) throw new TypeError(errors.table.replace("{received}", table));
-	if(!key) throw new TypeError(errors.key.replace("{received}", key));
-	
-	let keys = key.split('.'),
-	keys2 = key.split('.');
-	if(keys.length > 1){
-		key = keys.shift();
-	}
-	let res = await this.query({
-		sql: "SELECT value FROM " + table + " WHERE `key_name` = ?",
-		values: [key]
-	});
-	if(!res.length) return null;
-	let value = null;
-	if(res.length){
-		value = res[0].value;
-		if(!isNaN(value)){
-			value = Number(value);
+module.exports = async function (table, key, boolean) {
+	if (boolean === true || null) {
+
+		if (!table) throw new TypeError(errors.table.replace("{received}", table));
+		if (!key) throw new TypeError(errors.key.replace("{received}", key));
+
+		let keys = key.split('.'),
+			keys2 = key.split('.');
+		if (keys.length > 1) {
+			key = keys.shift();
 		}
-		try{
-			value = JSON.parse(value);
-		}catch(e){}
+		let res = await this.query({
+			sql: "SELECT value FROM " + table + " WHERE `key_name` = ?",
+			values: [key]
+		});
+		if (!res.length) return null;
+		let value = null;
+		if (res.length) {
+			value = res[0].value;
+			if (!isNaN(value)) {
+				value = Number(value);
+			}
+			try {
+				value = JSON.parse(value);
+			} catch (e) { }
+		}
+		if (keys2.length > 1 && typeof value === 'object') {
+			value = get(value, keys.join("."));
+			if (value == undefined) value = null;
+		} else if (keys2.length > 1) {
+			throw new ReferenceError(errors.targetNotObject.replace("{key}", key));
+		}
+		return value;
+	} else if (boolean === false) {
+		if (!table) {
+			throw new ReferenceError(errors.table.replace("{received}", table));
+		}
+		if (!key) {
+			throw new ReferenceError(errors.key.replace("{received}", key));
+		}
+		let db = this.db;
+		let keys = key.split('.');
+		let keys2 = key.split('.');
+		if (keys.length > 1) {
+			key = keys.shift();
+		}
+		let res = await db.query({
+			sql: "SELECT value FROM " + table + " WHERE `key_name` = ?",
+			values: [key]
+		});
+		let value = null;
+		if (res[0] && res[0].value) {
+			value = res[0].value;
+			if (!isNaN(value)) {
+				value = Number(value);
+			}
+			try {
+				value = JSON.parse(value);
+			} catch (e) { }
+		}
+		if (keys2.length > 1 && typeof value !== 'object') {
+			throw new ReferenceError(errors.targetNotObject.replace("{key}", key));
+		} else if (keys2.length > 1 && typeof value === 'object') {
+			value = get(value, keys.join(".")) || null;
+		}
+		return value;
 	}
-	if(keys2.length > 1 && typeof value === 'object'){
-		value = get(value, keys.join("."));
-		if(value == undefined) value = null;
-	}else if(keys2.length > 1){
-		throw new ReferenceError(errors.targetNotObject.replace("{key}", key));
-	}
-	return value;
+
 }

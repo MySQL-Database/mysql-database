cf3afda248d7232b8c165eac53136784c3cb4d89
abdooo9 <76847516+abdooo9@users.noreply.github.com>
2021-08-14 06:13:04 +0300

    No Update Just format Code 🙂

--- structures/errors/strings.js
+++ structures/errors/strings.js
@@ -5,15 +5,15 @@ module.exports = {
 	"number": "Number was not specified in function arguments, received {received}",
 	"numberType": "Number argument must be an positive integer, received {received}",
 	"query": "SQL Query was not specified in function arguments, received {received}",
-	
+
 	"array": "{key} key's value is not an array",
 	"notNumber": "{key} key's value is not a number",
 	"dataNotFound": "Data with the key {key} was not found",
-	
+
 	"targetNotObject": "Cannot target a non-object with the key {key}",
 	"variables": "Variables object argument was not specified in function arguments, received {received}",
 	"variablesNotObject": "Variables argument is not an object, received {received}",
 	"noTablesExisted": "No tables existed to get database ping, at least one table must be existed in the database",
-	
+
 	"baseNotString": "Base functions only works for strings"
 }

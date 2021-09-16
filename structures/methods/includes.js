04af8f9f44995b24af0fd7c00f2ad3727b981619
tariqkazem <tariqkazem@gmail.com>
2021-09-16 17:04:27 +0300

    Updated Includes Method

--- structures/methods/includes.js
+++ structures/methods/includes.js
@@ -7,11 +7,7 @@ module.exports = async function(table, key, value){
 	if(!key) throw new TypeError(errors.key.replace("{received}", key));
 	if(value == null) throw new TypeError(errors.value.replace("{received}", value));
 	
-	let res = false;
 	let data = await this.get(table, key) || [];
 	if(!Array.isArray(data)) throw new TypeError(errors.array.replace("{key}", key));
-	if(data.includes(value)){
-		res = true;
-	}
-	return res;
+	return data.includes(value);
 }

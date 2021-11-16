7faefb391d10c5e74e38c87b76e8419dd4370500
Abdooo9 <reset>
2021-11-16 10:28:41 +0300

    Undo the last commit

--- structures/methods/base_set.js
+++ structures/methods/base_set.js
@@ -5,7 +5,7 @@ const errors = require('../errors/strings.js');
 module.exports = async function(table, key, value){
 	if(!table) throw new TypeError(errors.table.replace("{received}", table));
 	if(!key) throw new TypeError(errors.key.replace("{received}", key));
-	if(!value) throw new TypeError(errors.value.replace("{received}", value));
+	if(value === undefined) throw new TypeError(errors.value.replace("{received}", value));
 	if(typeof value !== "string") throw new TypeError(errors.baseNotString);
 	
 	await this.set(table, key, Buffer.from(value, 'binary').toString('base64'));

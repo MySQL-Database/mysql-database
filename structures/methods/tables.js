715159686d54cfcae5cd5617b61d74f6ab509e2d
TARIQ <contact@itariq.dev>
2022-04-12 16:37:11 +0300

    Fix Undefined Tables If DB Name Includes UpperCase

--- structures/methods/tables.js
+++ structures/methods/tables.js
@@ -3,6 +3,6 @@
 const errors = require('../errors/strings.js');
 
 module.exports = async function(){
-	let tables = await this.query(`show tables;`);
-	return tables.map(m => m["Tables_in_" + this.db.pool.config.connectionConfig.database]);
+	let tables = await this.query(`SHOW TABLES;`);
+	return tables.map(m => m["Tables_in_" + this.db.pool.config.connectionConfig.database.toLowerCase()]);
 }

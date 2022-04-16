1365c159adb78ff9fbc51f2dbb0fc8777f049a14
TARIQ <contact@itariq.dev>
2022-04-16 16:41:33 +0300

    New (Only Push If Not Includes) Option

--- structures/methods/push.js
+++ structures/methods/push.js
@@ -2,20 +2,19 @@
 
 const errors = require('../errors/strings.js');
 
-module.exports = async function(table, key, value){
+module.exports = async function(table, key, value, option){
 	if(!table || typeof table !== "string") throw new TypeError(errors.table.replace("{received}", typeof table));
 	if(!key || typeof key !== "string") throw new TypeError(errors.key.replace("{received}", typeof key));
 	if(value === undefined) throw new TypeError(errors.value.replace("{received}", typeof value));
 	
-	let res;
-	let tables = await this.tables();
-	if(!tables.includes(table)){
-		res = await this.set(table, key, [value]);
+	await this.create(table);
+	
+	let data = await this.get(table, key) || [];
+	if(!Array.isArray(data)) throw new TypeError(errors.array.replace("{key}", key));
+	if(option === true){
+		if(!data.includes(value)) data.push(value);
 	}else{
-		let data = await this.get(table, key) || [];
-		if(!Array.isArray(data)) throw new TypeError(errors.array.replace("{key}", key));
 		data.push(value);
-		res = await this.set(table, key, data);
 	}
-	return res;
+	return this.set(table, key, data);
 }
